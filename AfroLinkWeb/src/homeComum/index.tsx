c9aecacb8256c8c9a4795120e6841ca397a87295
JasonAraujo1 <jasonaraujo123@hotmail.com>
2025-06-02 20:29:50 -0300

    fix: update municipio filter condition to use 'cidade' in HomeComum component

--- AfroLinkWeb/src/homeComum/index.tsx
+++ AfroLinkWeb/src/homeComum/index.tsx
@@ -13,14 +13,13 @@ export default function HomeComum() {
   const [dadosProfissionais, setDadosProfissionais] = useState([]);
 
   useEffect(() => {
-    console.log("Filtros recebidos:", filtros);
 
     const profissionaisFilter = dadosTodosUsers.filter(user => user.tipo === 'profissional');
 
     const filtroAplicado = profissionaisFilter.filter(user =>
       (!filtros?.profissao || user.profissao === filtros.profissao) &&
       (!filtros?.estado || user.estado === filtros.estado) &&
-      (!filtros?.municipio || user.municipio === filtros.municipio)
+      (!filtros?.municipio || user.cidade === filtros.municipio)
     );
 
     setDadosProfissionais(filtroAplicado);
