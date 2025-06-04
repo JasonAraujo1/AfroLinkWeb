6a22d8d17ec8eb10b6f634ec25d2e86d1d72f500
JasonAraujo1 <jasonaraujo123@hotmail.com>
2025-06-04 20:54:27 -0300

    feat: update HomeProfissional to filter usuarioComum and improve request display

--- AfroLinkWeb/src/homeProfissional/index.tsx
+++ AfroLinkWeb/src/homeProfissional/index.tsx
@@ -18,8 +18,10 @@ export default function HomeProfissional() {
   }, [])
   
   const solicitacoesFilter = todasSolicitacoes.filter((item)=>(item.id_usuario_profissional === dadosUser.id))
-  const usuarioComumFind = dadosTodosUsers.filter((item) => item.id === solicitacoesFilter.id_usuario_comum )
-  console.log('TESTE', usuarioComumFind)
+  const solicitacoesFilterUsuarioComum = todasSolicitacoes.map((item)=>(item.id_usuario_comum === dadosTodosUsers.id))
+  
+ console.log(dadosTodosUsers[14].id);
+
 
   
   return (
@@ -27,7 +29,7 @@ export default function HomeProfissional() {
       <h1></h1>
       {solicitacoesFilter.map((item) => (
         <div key={item.id}>
-          <h2>{item.data_solicitacao}</h2>
+          <h2>Data da solicitação: {item.data_solicitacao}</h2>
           <p>{item.status}</p>
         </div>
       ))}
