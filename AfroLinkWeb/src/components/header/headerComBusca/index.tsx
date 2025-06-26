ed6f0d27534ddd3400711b518b22b388323d93dd
JasonAraujo1 <jasonaraujo123@hotmail.com>
2025-06-26 11:13:38 -0300

    feat: refatorar componente Header e HeaderSimples; adicionar lógica de exibição condicional para usuário logado

--- AfroLinkWeb/src/components/header/headerComBusca/index.tsx
+++ AfroLinkWeb/src/components/header/headerComBusca/index.tsx
@@ -1,42 +1,12 @@
-import { NavLink, useNavigate } from 'react-router';
-import Logo from '../../../assets/logoWhite.png';
 import DropDownBusca from '../../../components/dropDownBusca';
 import IconesHome from '../../../components/iconesHome';
-import Context from '../../../context/Context';
-import { useContext } from 'react';
+import HeaderSimples from '../headerSimples';
 import './header.css'
-import MenuLateral from '../../menuLateral/menuLateral';
 
 export default function Header() {
-
-    const { dadosUser } = useContext(Context);
-
-
     return (
         <header className='headerHome fade-delay-2'>
-            <div className='topHeader fade-delay-3'>
-                <img src={Logo} className='Logo' alt="" />
-
-                <div className='linksDiv fade-in-left fade-delay-4'>
-                    {dadosUser ? (
-                        <div className='containerNomeSair'>
-                            <span className='userNome'>Olá, {dadosUser.nome_completo}</span>
-                            <span className='linha'></span>
-                            <MenuLateral/>
-                        </div>
-                    ) : (
-                        <>
-                            <NavLink className='linksHeader fade-in-left fade-delay-5' to={'/cadastro'}>
-                                Cadastro
-                            </NavLink>
-                            <NavLink className='linksHeader fade-in-left fade-delay-5' to={'/login'}>
-                                Login
-                            </NavLink>
-                        </>
-                    )}
-                </div>
-            </div>
-
+            <HeaderSimples />
             <h1 className='tituloHome '>
                 ENCONTRE UM PROFISSIONAL<span className='tituloHome_span'>NEGRO</span>
             </h1>
--- AfroLinkWeb/src/components/header/headerSimples/index.tsx
+++ AfroLinkWeb/src/components/header/headerSimples/index.tsx
@@ -1,14 +1,39 @@
 import { NavLink } from 'react-router';
 import Logo from '../../../assets/logoWhite.png';
 import './headerSimples.css'
+import Context from '../../../context/Context';
+import { useContext } from 'react';
+import MenuLateral from '../../menuLateral/menuLateral';
+
 
 export default function HeaderSimples() {
+  const { dadosUser } = useContext(Context);
 
   return (
     <header className='headerHome headerSimples fade-delay-2'>
-      <div className='topHeaderSimples fade-delay-3 '>
-        <img src={Logo} className='Logo ' alt="" />
-        <NavLink className='linkHome' to={'/'}>Home</NavLink>
+      <div className='topHeaderSimples fade-delay-3'>
+        <div className='logoAndLink fade-delay-3 fade-in-left'>
+          <img src={Logo} className='Logo' alt="" />
+          <NavLink className='linkHome' to={'/'}>Home</NavLink>
+        </div>
+        <div className='linksDiv fade-in-left fade-delay-4'>
+          {dadosUser ? (
+            <div className='containerNomeSair'>
+              <span className='userNome'>Olá, {dadosUser.nome_completo}</span>
+              <span className='linha'></span>
+              <MenuLateral />
+            </div>
+          ) : (
+            <>
+              <NavLink className='linksHeader fade-in-left fade-delay-5' to={'/cadastro'}>
+                Cadastro
+              </NavLink>
+              <NavLink className='linksHeader fade-in-left fade-delay-5' to={'/login'}>
+                Login
+              </NavLink>
+            </>
+          )}
+        </div>
       </div>
     </header>
   )
